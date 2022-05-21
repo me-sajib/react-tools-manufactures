85e954b0c4c52b89f9cd45f9dc2c70349e98b762
sajib <me.mrsajib@gmail.com>
2022-05-21 22:25:25 +0600

    navbar set and route set

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
+import Navbar from "./Pages/Shared/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1 className="text-5xl text-primary">hey tailwind css</h1>
     </div>
   );
