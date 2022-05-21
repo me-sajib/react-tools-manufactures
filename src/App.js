572b458c8f73677e85946363815cd72211033bff
sajib <me.mrsajib@gmail.com>
2022-05-21 21:50:42 +0600

    themes color add

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1 className="text-5xl">hey tailwind css</h1>
+      <h1 className="text-5xl text-primary">hey tailwind css</h1>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,4 +5,19 @@ module.exports = {
   },
 
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        doctortheme: {
+          primary: "#0FCFEC",
+          secondary: "#19D3AE",
+          accent: "#3A4256",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
 };
