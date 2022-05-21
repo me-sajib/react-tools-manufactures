1c815b87dca0826485d983c241148616e6eeff48
sajib <me.mrsajib@gmail.com>
2022-05-21 23:28:09 +0600

    color chagne

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,8 @@ module.exports = {
     themes: [
       {
         doctortheme: {
-          primary: "#0FCFEC",
-          secondary: "#19D3AE",
+          primary: "#202447",
+          secondary: "#fab915",
           accent: "#3A4256",
           neutral: "#3d4451",
           "base-100": "#ffffff",
