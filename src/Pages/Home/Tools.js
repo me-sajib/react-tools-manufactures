066d1a9c8cddc20e0439b55af90d314196e16efb
sajib <me.mrsajib@gmail.com>
2022-05-22 16:39:42 +0600

    all tools product show and fetch to react query

--- src/Pages/Home/Tools.js
+++ src/Pages/Home/Tools.js
@@ -1,86 +1,19 @@
 import React from "react";
+import { useQuery } from "react-query";
+import ToolsProduct from "./ToolsProduct";
 
 const Tools = () => {
+  const { data: tools, isLoading } = useQuery("tools", () =>
+    fetch("tools.json").then((res) => res.json())
+  );
+  if (isLoading) return <p>Loading...</p>;
+  console.log(tools);
   return (
     <div className="container-width">
       <div className="my-16 grid lg:grid-cols-3 sm:grid-cols-1 md:grid-cols-2 gap-5">
-        <div class="card w-96 bg-base-100 shadow-xl">
-          <figure>
-            <img
-              src="https://api.lorem.space/image/shoes?w=400&h=225"
-              alt="Shoes"
-            />
-          </figure>
-          <div class="card-body">
-            <h2 class="card-title">
-              Shoes!
-              <div class="badge badge-secondary">NEW</div>
-            </h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
-            <div class="card-actions justify-end">
-              <div class="badge badge-outline">Fashion</div>
-              <div class="badge badge-outline">Products</div>
-            </div>
-          </div>
-        </div>
-
-        <div class="card w-96 bg-base-100 shadow-xl">
-          <figure>
-            <img
-              src="https://api.lorem.space/image/shoes?w=400&h=225"
-              alt="Shoes"
-            />
-          </figure>
-          <div class="card-body">
-            <h2 class="card-title">
-              Shoes!
-              <div class="badge badge-secondary">NEW</div>
-            </h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
-            <div class="card-actions justify-end">
-              <div class="badge badge-outline">Fashion</div>
-              <div class="badge badge-outline">Products</div>
-            </div>
-          </div>
-        </div>
-        <div class="card w-96 bg-base-100 shadow-xl">
-          <figure>
-            <img
-              src="https://api.lorem.space/image/shoes?w=400&h=225"
-              alt="Shoes"
-            />
-          </figure>
-          <div class="card-body">
-            <h2 class="card-title">
-              Shoes!
-              <div class="badge badge-secondary">NEW</div>
-            </h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
-            <div class="card-actions justify-end">
-              <div class="badge badge-outline">Fashion</div>
-              <div class="badge badge-outline">Products</div>
-            </div>
-          </div>
-        </div>
-        <div class="card w-96 bg-base-100 shadow-xl">
-          <figure>
-            <img
-              src="https://api.lorem.space/image/shoes?w=400&h=225"
-              alt="Shoes"
-            />
-          </figure>
-          <div class="card-body">
-            <h2 class="card-title">
-              Shoes!
-              <div class="badge badge-secondary">NEW</div>
-            </h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
-            <div class="card-actions justify-end">
-              <div class="badge badge-outline">Fashion</div>
-              <div class="badge badge-outline">Products</div>
-            </div>
-          </div>
-        </div>
+        {tools.map((tool) => (
+          <ToolsProduct tools={tool} key={tool.price} />
+        ))}
       </div>
     </div>
   );
---
+++ src/Pages/Home/ToolsProduct.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./css/Tools.css";
+
+const ToolsProduct = ({ tools }) => {
+  const { _id, name, price, description, minimumOrder, available, image } =
+    tools;
+  return (
+    <div class="card w-96 bg-base-100 shadow-xl">
+      <figure>
+        <img src={image} alt="Shoes" className="toolsImage" />
+      </figure>
+      <div class="card-body">
+        <h2 class="card-title font-bold">{name}</h2>
+        <h3 className="text-2xl font-semibold">price: ${price}</h3>
+        <p className="text-xl">Available: {available}</p>
+        <p className="text-xl">Minimum Order: {minimumOrder}</p>
+        <p>{description}</p>
+        <div class="card-actions justify-end">
+          <Link to={`/purchase/${_id}`} className="btn btn-primary">
+            purchase
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ToolsProduct;
