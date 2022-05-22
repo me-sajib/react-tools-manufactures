db00ed7f04c38a39e378ebdbab89bd19fa852037
sajib <me.mrsajib@gmail.com>
2022-05-23 01:38:54 +0600

    review item show compoenents add

---
+++ src/Pages/Home/Review.js
@@ -0,0 +1,35 @@
+import React from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+// import required modules
+import { Pagination } from "swiper";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "./css/Review.css";
+
+const Review = () => {
+  return (
+    <div className="container-width">
+      <h2 className="text-3xl">Review</h2>
+      <Swiper
+        slidesPerView={3}
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+      </Swiper>
+    </div>
+  );
+};
+
+export default Review;
