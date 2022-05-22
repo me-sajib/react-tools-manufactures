0adbcb344e9ded03c3b836589b13c4c2d98d846c
sajib <me.mrsajib@gmail.com>
2022-05-22 17:27:39 +0600

    route set

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import Home from "./Pages/Home/Home";
 import "./App.css";
 import Footer from "./Pages/Shared/Footer";
+import Login from "./Pages/Login/Login";
+import PrivateRoute from "./Auth/PrivateRoute";
+import Purchase from "./Pages/Purchase/Purchase";
+import Navbar from "./Pages/Shared/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Registration from "./Pages/Login/Registration";
 
 function App() {
   return (
-    <>
-      <Home />
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/purchase/:id"
+          element={
+            <PrivateRoute>
+              <Purchase />
+            </PrivateRoute>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/registration" element={<Registration />} />
+      </Routes>
       <Footer />
-    </>
+    </div>
   );
 }
 
