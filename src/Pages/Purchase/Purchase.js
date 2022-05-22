8b57f210364ee095ff64ec22790cb93fce481c70
sajib <me.mrsajib@gmail.com>
2022-05-22 16:49:48 +0600

    add to purchase pages

---
+++ src/Pages/Purchase/Purchase.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+const Purchase = () => {
+  const { id } = useParams();
+  return <div></div>;
+};
+
+export default Purchase;
