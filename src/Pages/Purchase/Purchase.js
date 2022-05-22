d3cd0cd8fd1c0c368364e2d8a03187cea06e34cb
sajib <me.mrsajib@gmail.com>
2022-05-22 18:27:12 +0600

    show tools by id

--- src/Pages/Purchase/Purchase.js
+++ src/Pages/Purchase/Purchase.js
@@ -1,9 +1,59 @@
 import React from "react";
+import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
-
 const Purchase = () => {
   const { id } = useParams();
-  return <div></div>;
+  const { data: tool, isLoading } = useQuery(["tools", id], () =>
+    fetch(`http://localhost:5000/tools/${id}`).then((res) => res.json())
+  );
+  if (isLoading) return <p>Loading...</p>;
+  return (
+    <div className="container-width">
+      <div class="hero  bg-base-100 my-24">
+        <div class="hero-content flex-col lg:flex-row">
+          <img src={tool.image} class="max-w-sm rounded-lg shadow-2xl" alt="" />
+          <div>
+            <h1 class="text-5xl font-bold">{tool.name}</h1>
+            <p class="py-6">{tool.description}</p>
+            <h3 className="text-2xl font-semibold uppercase">
+              price: ${tool.price}
+            </h3>
+            <p className="text-xl py-2">
+              Minimum Order:{" "}
+              <span className="text-orange-500 font-bold">
+                {tool.minimumOrder}
+              </span>
+            </p>
+            <p className="text-xl pb-4">
+              Available:{" "}
+              <span className="text-blue-500 font-bold">{tool.available}</span>
+            </p>
+            <label for="purchase-modal" class="btn modal-button">
+              open modal
+            </label>
+          </div>
+        </div>
+        {/* <!-- Put this part before </body> tag --> */}
+        <input type="checkbox" id="purchase-modal" class="modal-toggle" />
+        <div class="modal modal-bottom sm:modal-middle">
+          <div class="modal-box">
+            <h3 class="font-bold text-lg">
+              Congratulations random Interner user!
+            </h3>
+            <p class="py-4">
+              You've been selected for a chance to get one year of subscription
+              to use Wikipedia for free!
+            </p>
+            <div class="modal-action">
+              <label for="purchase-modal" class="btn">
+                Yay!
+              </label>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Purchase;
