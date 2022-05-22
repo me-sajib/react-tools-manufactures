a85d0de97b65d37211f5e3da10d9972755f33e51
sajib <me.mrsajib@gmail.com>
2022-05-22 18:27:25 +0600

    set id to route

--- src/Pages/Home/Tools.js
+++ src/Pages/Home/Tools.js
@@ -4,7 +4,7 @@ import ToolsProduct from "./ToolsProduct";
 
 const Tools = () => {
   const { data: tools, isLoading } = useQuery("tools", () =>
-    fetch("tools.json").then((res) => res.json())
+    fetch("http://localhost:5000/tools").then((res) => res.json())
   );
   if (isLoading) return <p>Loading...</p>;
   console.log(tools);
@@ -12,7 +12,7 @@ const Tools = () => {
     <div className="container-width">
       <div className="my-16 grid lg:grid-cols-3 sm:grid-cols-1 md:grid-cols-2 gap-5">
         {tools.map((tool) => (
-          <ToolsProduct tools={tool} key={tool.price} />
+          <ToolsProduct tools={tool} key={tool._id} />
         ))}
       </div>
     </div>
