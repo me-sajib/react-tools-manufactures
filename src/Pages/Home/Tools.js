e01b7ff11a3f19fc959e162f8e103ec03e282bb4
sajib <me.mrsajib@gmail.com>
2022-05-23 12:12:34 +0600

    update

--- src/Pages/Home/Tools.js
+++ src/Pages/Home/Tools.js
@@ -7,7 +7,6 @@ const Tools = () => {
     fetch("http://localhost:5000/tools").then((res) => res.json())
   );
   if (isLoading) return <p>Loading...</p>;
-  console.log(tools);
   return (
     <div className="container-width">
       <div className="my-16 grid lg:grid-cols-3 sm:grid-cols-1 md:grid-cols-2 gap-5">
