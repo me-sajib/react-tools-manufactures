41dba62f8726e4e559a9455e5313eecc01074318
sajib <me.mrsajib@gmail.com>
2022-05-23 22:40:05 +0600

    add color in text

--- src/Pages/Dashboard/Admin/OrderTable.js
+++ src/Pages/Dashboard/Admin/OrderTable.js
@@ -1,14 +1,19 @@
 import React from "react";
 
 const OrderTable = ({ order, index, refetch }) => {
-  console.log(order);
   return (
     <tr>
       <td>{index + 1}</td>
       <td>{order?.email}</td>
       <td>{order?.toolName}</td>
       <td>pending</td>
-      <td>{order?.paid ? "paid" : "unpaid"}</td>
+      <td>
+        {order?.paid ? (
+          <span className="btn btn-xs btn-success"> paid</span>
+        ) : (
+          "unpaid"
+        )}
+      </td>
       <td>
         <button className="btn btn-xs btn-primary">Cancel</button>
       </td>
