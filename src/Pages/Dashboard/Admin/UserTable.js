6946be31c27952cef525aa1041294c93a6c31a53
sajib <me.mrsajib@gmail.com>
2022-05-23 17:00:53 +0600

    all user show

---
+++ src/Pages/Dashboard/Admin/UserTable.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const UserTable = ({ user, refetch, index }) => {
+  return (
+    <tr>
+      <td>{index + 1}</td>
+      <td>{user?.email}</td>
+      <td>
+        <button>make admin</button>
+      </td>
+      <td>
+        <button className="btn btn-secondary btn-sm">delete</button>
+      </td>
+    </tr>
+  );
+};
+
+export default UserTable;
---
+++ src/Pages/Dashboard/Admin/Users.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { useQuery } from "react-query";
+import Spinner from "../../Shared/Spinner";
+import UserTable from "./UserTable";
+
+const Users = () => {
+  const {
+    data: user,
+    isLoading,
+    refetch,
+  } = useQuery(["user"], () =>
+    fetch("http://localhost:5000/users", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        authorization: `Bearer ${localStorage.getItem("access")}`,
+      },
+    }).then((res) => res.json())
+  );
+  console.log(user);
+  if (isLoading) return <Spinner />;
+  return (
+    <div>
+      {/* show information */}
+      <div class="overflow-x-auto">
+        <table class="table w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Email</th>
+              <th>Make admin</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {user.map((user, index) => (
+              <UserTable
+                user={user}
+                key={user._id}
+                index={index}
+                refetch={refetch}
+              />
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default Users;
