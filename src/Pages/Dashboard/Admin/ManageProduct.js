9aeba7c2a77b8b04c3487ccb20b481978e3b1648
sajib <me.mrsajib@gmail.com>
2022-05-24 12:22:12 +0600

    manage product page create

---
+++ src/Pages/Dashboard/Admin/ManageProduct.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ManageProduct = () => {
+  return (
+    <div>
+      <h3 className="text-3xl">manage products</h3>
+    </div>
+  );
+};
+
+export default ManageProduct;
