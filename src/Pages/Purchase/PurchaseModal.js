f6854db1494692fcda51a056a4bf3c28c2ce0ee8
sajib <me.mrsajib@gmail.com>
2022-05-24 17:21:59 +0600

    change the modal style

--- src/Pages/Purchase/PurchaseModal.js
+++ src/Pages/Purchase/PurchaseModal.js
@@ -3,6 +3,7 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import auth from "../../firebase.config";
 import Spinner from "../Shared/Spinner";
 import Swal from "sweetalert2";
+import "./css/Purchase.css";
 
 const PurchaseModal = ({ tool }) => {
   const [user, loading] = useAuthState(auth);
@@ -62,114 +63,107 @@ const PurchaseModal = ({ tool }) => {
   if (loading) return <Spinner />;
   return (
     <div>
-      {/* <!-- Put this part before </body> tag --> */}
-      <input type="checkbox" id="purchase-modal" class="modal-toggle" />
-      <div class="modal modal-bottom sm:modal-middle">
-        <div class="modal-box">
-          <label
-            htmlFor="purchase-modal"
-            className="btn btn-sm btn-circle absolute right-2 top-2"
-          >
-            ✕
-          </label>
-          <h3 class="font-bold text-3xl uppercase text-blue-500">
-            You have purchase to {name}
-          </h3>
-          <div class="py-4">
+      <div
+        class="hero min-h-screen"
+        style={{ backgroundImage: `url(${tool.image})` }}
+      >
+        <div class="hero-overlay bg-opacity-90"></div>
+        <div class="hero-content text-center text-neutral-content">
+          <div class="max-w-md">
+            <h3 class="font-bold mb-5 text-3xl uppercase text-primary-500">
+              You have purchase to <em> {name}</em>
+            </h3>
             <h2 className="text-2xl uppercase pb-5 text-secondary">
               Order Details
             </h2>
-            {/* form to details user send to tools */}
             <form
               onSubmit={purchaseTool}
               className="grid grid-cols-1 gap-3 mt-2"
             >
               {/* current user name */}
-              <div className="form-control w-full mx-w-xs">
-                <label htmlFor="" className="label">
-                  <span className="label-text">Name</span>
+              <div className="form-control w-full mx-w-md  text-black">
+                <label htmlFor="" className="label ">
+                  <span className="label-text text-white">Name</span>
                 </label>
                 <input
                   type="text"
-                  disabled
+                  readOnly
                   name="name"
                   value={user?.displayName}
-                  class="input input-bordered input-md w-full max-w-xs"
+                  class="input input-bordered input-md w-full max-w-md"
                 />
               </div>
 
               {/* current user email */}
-              <div className="form-control w-full mx-w-xs">
+              <div className="form-control w-full mx-w-md text-black">
                 <label htmlFor="" className="label">
-                  <span className="label-text">Email</span>
+                  <span className="label-text text-white">Email</span>
                 </label>
                 <input
                   type="email"
                   name="email"
-                  disabled
+                  readOnly
                   value={user?.email}
                   placeholder="your email"
-                  class="input input-bordered input-md w-full max-w-xs"
+                  class="input input-bordered  input-md w-full max-w-md"
                 />
               </div>
 
               {/* user phone number */}
-              <div className="form-control w-full mx-w-xs">
+              <div className="form-control w-full mx-w-md">
                 <label htmlFor="" className="label">
-                  <span className="label-text">Phone</span>
+                  <span className="label-text text-white">Phone</span>
                 </label>
                 <input
                   type="number"
                   name="phone"
                   placeholder="your phone number"
-                  class="input input-bordered input-md w-full max-w-xs"
+                  class="input input-bordered input-md w-full max-w-md"
                 />
               </div>
 
               {/* user tools orders  number */}
-              <div className="form-control w-full mx-w-xs">
+              <div className="form-control w-full mx-w-md text-black">
                 <label htmlFor="" className="label">
-                  <span className="label-text">Order Quantity</span>
+                  <span className="label-text text-white">Order Quantity</span>
                 </label>
                 <input
                   type="number"
                   name="orderCount"
                   value={orderCount}
                   onChange={(e) => setOrderCount(e.target.value)}
-                  class="input input-bordered input-md w-full max-w-xs"
+                  class="input input-bordered input-md w-full max-w-md"
                 />
-                {orderError && <p className="text-red-500">{orderError}</p>}
+                {orderError && (
+                  <p className="text-red-500 text-left">{orderError}</p>
+                )}
               </div>
-              <div className="form-control w-full mx-w-xs">
+              <div className="form-control w-full mx-w-md text-black">
                 <label htmlFor="" className="label">
-                  <span className="label-text">Address</span>
+                  <span className="label-text text-white">Address</span>
                 </label>
                 <textarea
                   type="text"
                   name="address"
                   placeholder="your shipping address"
-                  class="input input-bordered input-md w-full max-w-xs"
+                  class="input input-bordered input-md w-full max-w-md"
                 />
               </div>
-              <input
-                type="submit"
-                value="Submit"
-                className="btn btn-primary w-full max-w-xs"
-              />
-              <div class="modal-action">
-                {!orderError ? (
-                  <label
-                    type="submit"
-                    id="purchase-modal"
-                    className="btn"
-                    htmlFor="purchase-modal"
-                  >
-                    buy now
-                  </label>
-                ) : (
-                  <label class="btn btn-disabled">Buy now</label>
-                )}
-              </div>
+
+              {!orderError ? (
+                <input
+                  type="button"
+                  value="Submit"
+                  className="btn btn-primary w-full max-w-md"
+                />
+              ) : (
+                <input
+                  type="button"
+                  value="Submit"
+                  className=" text-white btn error-btn w-full  max-w-md"
+                  disabled
+                />
+              )}
             </form>
           </div>
         </div>
