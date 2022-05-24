837f52fc6847c9c3ef218e1d9670b0e7b8529e42
sajib <me.mrsajib@gmail.com>
2022-05-24 15:14:28 +0600

    show all product

---
+++ src/Pages/Dashboard/Admin/AllProduct.js
@@ -0,0 +1,67 @@
+import React from "react";
+import Swal from "sweetalert2";
+
+const AllProduct = ({ product, index, refetch }) => {
+  // delete product api call
+  const deleteProduct = (id) => {
+    // show confirm
+    Swal.fire({
+      title: "Are you sure?",
+      text: "Are you sure want to cancel this order?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, cancel it!",
+    }).then((result) => {
+      // is confirm button clicked
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/tools/${id}`, {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+            authorization: `Bearer ${localStorage.getItem("access")}`,
+          },
+          body: JSON.stringify({ id }),
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.deletedCount) {
+              Swal.fire(
+                "Deleted!",
+                "Your product has been Deleted.",
+                "success"
+              );
+              refetch();
+            }
+          });
+      }
+    });
+  };
+
+  return (
+    <tr>
+      <td>{index + 1}</td>
+      <td>{product?.name}</td>
+      <td>${product?.price}</td>
+      <td>{product?.available}</td>
+      <td>
+        <div class="avatar">
+          <div class="w-8 rounded">
+            <img src={product?.image} alt="Tailwind-CSS-Avatar-component" />
+          </div>
+        </div>
+      </td>
+      <td>
+        <button
+          className="btn btn-secondary btn-xs"
+          onClick={() => deleteProduct(product._id)}
+        >
+          delete
+        </button>
+      </td>
+    </tr>
+  );
+};
+
+export default AllProduct;
