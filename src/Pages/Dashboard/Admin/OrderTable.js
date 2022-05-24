865944d10fd4bda9da9b089105e3c0d0241e1312
sajib <me.mrsajib@gmail.com>
2022-05-24 11:24:32 +0600

    delete any order successfully

--- src/Pages/Dashboard/Admin/OrderTable.js
+++ src/Pages/Dashboard/Admin/OrderTable.js
@@ -1,12 +1,73 @@
 import React from "react";
+import { useQuery } from "react-query";
+import Swal from "sweetalert2";
+import Spinner from "../../Shared/Spinner";
 
 const OrderTable = ({ order, index, refetch }) => {
+  const { data: status, isLoading } = useQuery("status", () =>
+    fetch("http://localhost:5000/order", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        authorization: `Bearer ${localStorage.getItem("access")}`,
+      },
+    }).then((res) => res.json())
+  );
+
+  if (isLoading) return <Spinner />;
+
+  const orderStatus = (e) => {
+    e.preventDefault();
+    console.log(e.target.value);
+  };
+  // delete order
+  const deleteOrder = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "Are you sure want to delete this order?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes",
+    }).then((result) => {
+      // is confirm button clicked
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/order/${id}`, {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+            authorization: `Bearer ${localStorage.getItem("access")}`,
+          },
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.deletedCount) {
+              Swal.fire("Deleted!", "Order has been deleted.", "success");
+              refetch();
+            }
+          });
+      }
+    });
+  };
+
   return (
     <tr>
       <td>{index + 1}</td>
       <td>{order?.email}</td>
       <td>{order?.toolName}</td>
-      <td>pending</td>
+      <td>
+        <form onChange={orderStatus}>
+          <select class="select w-full max-w-xs" name="order">
+            <option disabled selected>
+              Order Status
+            </option>
+            <option>Pending</option>
+            <option>Shifted</option>
+            <option>Delivery</option>
+          </select>
+        </form>
+      </td>
       <td>
         {order?.paid ? (
           <span className="btn btn-xs btn-success"> paid</span>
@@ -15,7 +76,12 @@ const OrderTable = ({ order, index, refetch }) => {
         )}
       </td>
       <td>
-        <button className="btn btn-xs btn-primary">Cancel</button>
+        <button
+          className="btn btn-xs btn-primary"
+          onClick={() => deleteOrder(order._id)}
+        >
+          Cancel
+        </button>
       </td>
     </tr>
   );
