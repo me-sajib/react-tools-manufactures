77694b30e7b855d5b57ab8b5a0e9fddb51d62cbc
sajib <me.mrsajib@gmail.com>
2022-05-25 22:33:03 +0600

    save the env file

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDyRzHrdWXtPfdbW3Vrbym9xxcwUNwIS6o",
-  authDomain: "menufacture-ec58d.firebaseapp.com",
-  projectId: "menufacture-ec58d",
-  storageBucket: "menufacture-ec58d.appspot.com",
-  messagingSenderId: "518845231463",
-  appId: "1:518845231463:web:8a5787165d8c087d71ffc2",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
