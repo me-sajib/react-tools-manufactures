2f691cfe4d3ccc5fdd3ebe099ab380c10c0f5411
sajib <me.mrsajib@gmail.com>
2022-05-25 21:35:37 +0600

    delete user by email

--- src/Pages/Dashboard/Admin/UserTable.js
+++ src/Pages/Dashboard/Admin/UserTable.js
@@ -1,9 +1,9 @@
 import React from "react";
 import Swal from "sweetalert2";
 
-const UserTable = ({ user, refetch, index }) => {
+const UserTable = ({ users, refetch, currentUser, index }) => {
   const createAdmin = () => {
-    fetch(`http://localhost:5000/user/admin/${user.email}`, {
+    fetch(`http://localhost:5000/user/admin/${users.email}`, {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
@@ -23,12 +23,47 @@ const UserTable = ({ user, refetch, index }) => {
         }
       });
   };
+
+  // delete user
+  const deleteUser = () => {
+    // confirm to delete user show swl alert
+    Swal.fire({
+      title: "Are you sure?",
+      text: "Are you sure want to delete this user?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes",
+    }).then((result) => {
+      // is confirm button clicked
+      if (result.isConfirmed) {
+        const email = users.email;
+        fetch(`http://localhost:5000/user/${email}`, {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+            authorization: `Bearer ${localStorage.getItem("access")}`,
+          },
+          body: JSON.stringify({ email }),
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.deletedCount > 0) {
+              refetch();
+              Swal.fire("Deleted!", "User has been deleted.", "success");
+            }
+          });
+      }
+    });
+  };
+
   return (
     <tr>
       <td>{index + 1}</td>
-      <td>{user?.email}</td>
+      <td>{users?.email}</td>
       <td>
-        {user?.role !== "admin" ? (
+        {users?.role !== "admin" ? (
           <button className="btn btn-xs btn-secondary" onClick={createAdmin}>
             make admin
           </button>
@@ -37,7 +72,15 @@ const UserTable = ({ user, refetch, index }) => {
         )}
       </td>
       <td>
-        <button className="btn btn-secondary btn-sm">delete</button>
+        {users?.email === currentUser?.email ? (
+          <button className="btn btn-secondary btn-sm" disabled>
+            delete
+          </button>
+        ) : (
+          <button className="btn btn-secondary btn-sm" onClick={deleteUser}>
+            delete
+          </button>
+        )}
       </td>
     </tr>
   );
