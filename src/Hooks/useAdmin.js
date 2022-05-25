1878ce959d4d00e40f4eca2ee3ace49fa16299d8
sajib <me.mrsajib@gmail.com>
2022-05-25 21:33:38 +0600

    add loading

--- src/Hooks/useAdmin.js
+++ src/Hooks/useAdmin.js
@@ -2,24 +2,19 @@ import { useEffect, useState } from "react";
 
 const useAdmin = (user) => {
   const [admin, setAdmin] = useState(false);
-  const [adminLoading, setAdminLoading] = useState(true);
+  const [loadingAdminCheck, setLoadingAdminCheck] = useState(true);
   useEffect(() => {
     const email = user?.email;
     if (email) {
-      fetch(`http://localhost:5000/admin/${email}`, {
-        method: "GET",
-        headers: {
-          "content-type": "application/json",
-          authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-        },
-      })
+      fetch(`http://localhost:5000/admin/${email}`)
         .then((res) => res.json())
         .then((result) => {
           setAdmin(result.admin);
-          setAdminLoading(false);
+          setLoadingAdminCheck(false);
         });
     }
   }, [user]);
-  return [admin];
+
+  return [admin, loadingAdminCheck];
 };
 export default useAdmin;
