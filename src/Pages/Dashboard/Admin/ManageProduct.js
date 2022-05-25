c5598238e7cba6ff4a94cded5d7a0c61784cfc83
sajib <me.mrsajib@gmail.com>
2022-05-25 22:07:43 +0600

    spinner add

--- src/Pages/Dashboard/Admin/ManageProduct.js
+++ src/Pages/Dashboard/Admin/ManageProduct.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useQuery } from "react-query";
+import Spinner from "../../Shared/Spinner";
 import AllProduct from "./AllProduct";
 
 const ManageProduct = () => {
@@ -11,7 +12,8 @@ const ManageProduct = () => {
     fetch("http://localhost:5000/tools").then((res) => res.json())
   );
 
-  if (isLoading) return <div>Loading...</div>;
+  if (isLoading) return <Spinner />;
+
   return (
     <div>
       <h2 className="text-3xl text-orange-500 uppercase py-5">
--- src/Pages/Shared/Spinner.js
+++ src/Pages/Shared/Spinner.js
@@ -4,10 +4,10 @@ const Spinner = () => {
   return (
     <div className="flex items-center justify-center">
       <div
-        className="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full"
+        className="spinner-border animate-spin inline-block w-12 h-12 border-4 rounded-full"
         role="status"
       >
-        <span className="visually-hidden">Loading...</span>
+        <span className="visually-hidden"></span>
       </div>
     </div>
   );
