d25dc2b7f90fc0a61bb3baf0655701a86373ad77
sajib <me.mrsajib@gmail.com>
2022-05-25 21:57:08 +0600

    change the spinner style

--- src/Pages/Shared/Spinner.js
+++ src/Pages/Shared/Spinner.js
@@ -2,8 +2,13 @@ import React from "react";
 
 const Spinner = () => {
   return (
-    <div>
-      <p>Loading........</p>
+    <div class="flex items-center justify-center">
+      <div
+        class="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full"
+        role="status"
+      >
+        <span class="visually-hidden">Loading...</span>
+      </div>
     </div>
   );
 };
